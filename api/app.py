d2dca860ac820cfce1b62e750fe40c4596f48068
piotr <upsol@protonmail.com>
2022-07-30 13:10:39 +0200

    renaming in app

--- api/app.py
+++ api/app.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI, BackgroundTasks
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from api.controllers import thread_json, thread_pdf, delete_pdf, user_json, user_data
 
 app = FastAPI()
 
+origins = ["http://localhost:5173"]
+app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["GET", "POST"])
+
 @app.get("/ok")
 def health_route():
-     return {"message": "Server is running!"}
+    return {"message": "Server is running!"}
 
-@app.get("/thread/json")
-async def get_json(id):   
+
+@app.post("/thread/json")
+async def get_json(id):
     res = await thread_json(id)
     return res
 
-@app.get("/thread", response_class=FileResponse)
+
+@app.post("/thread", response_class=FileResponse)
 async def get_pdf(background_tasks: BackgroundTasks, id):
     background_tasks.add_task(delete_pdf, id)
     res = await thread_pdf(id)
     return res
-
 
 # TODO
 # @app.get("/user")
@@ -26,10 +31,9 @@ async def get_pdf(background_tasks: BackgroundTasks, id):
 #     res = await user_data(id)
 #     return res
 
-@app.get("/user/archive")
+@app.post("/user/archive")
 async def get_json_archive(id, limit=10):
     res = await user_json(id, int(limit))
     return res
-    
 
 # TODO Scrap Only media files of thread
