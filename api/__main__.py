53f1713391e6fac72c6f0ae6d8ab1ff0f2d2d23e
piotr <upsol@protonmail.com>
2022-08-03 15:56:21 +0200

    nginx config

--- api/__main__.py
+++ api/__main__.py
@@ -8,4 +8,4 @@ HOST = os.environ.get("HOST")
 
 if __name__ == "__main__":
     print("Starting server...")
-    uvicorn.run("api.app:app", host=HOST, port=int(PORT), reload=True)
+    uvicorn.run("api.app:app", host=HOST, port=int(PORT), reload=True, proxy_headers=True)
